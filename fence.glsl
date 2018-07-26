90c05648137fc27d08dee889ca9ee43cbb7c5744
Ilyas Tazitdinov <itazitdinov@acumatica.com>
2018-07-26 13:05:10 +0300

    first steps

---
+++ fence.glsl
@@ -0,0 +1,25 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+// Plot a line on Y using a value between 0.0-1.0
+float plot(vec2 coords, float y) {
+  return smoothstep(y - 0.02, y, coords.y) - smoothstep(y, y + 0.02, coords.y);
+}
+
+void main() {
+  vec2 st = gl_FragCoord.xy / iResolution.xy;
+
+  // float y = pow(st.x, 5.0);
+  // float y = st.x;
+  // float y = smoothstep(0.2,0.5,st.x) - smoothstep(0.5,0.8,st.x);
+  float y = sin(iGlobalTime + st.x*3.14159265359*4.) / 2. + .5;
+
+  vec3 color = vec3(y);
+
+  // Plot a line
+  float pct = plot(st, y);
+  color = (1.0 - pct) * color + pct * vec3(0.0, 1.0, 0.0);
+
+  gl_FragColor = vec4(color, 1.0);
+}
---
+++ simple.glsl
@@ -0,0 +1,23 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+float PI = 3.14159265359;
+// Plot a line on Y using a value between 0.0-1.0
+float plot(vec2 coords, float y) {
+  return smoothstep(y - 0.02, y, coords.y) - smoothstep(y, y + 0.02, coords.y);
+}
+
+void main() {
+  vec2 st = gl_FragCoord.xy / iResolution.xy;
+  st = (st - .5) * 4.;
+  float x = st.x;
+
+  //float y = sin(st.x*3.14159265359);
+  //float y = 1. - pow(abs(x), .5);
+  float y = pow(cos(PI *x), 0.5);
+  // Plot a line
+  float pct = plot(st, y);
+  vec3 color = (1.0 - pct) * vec3(0.0, 0.0, 0.0) + pct * vec3(0.0, .5, .5);
+
+  gl_FragColor = vec4(color, 1.0);
+}
