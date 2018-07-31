70e4b28acb501ae6d6f2db2176c3756df2367cf5
Ilyas Tazitdinov <itazitdinov@acumatica.com>
2018-08-01 01:11:21 +0300

    random

---
+++ random.glsl
@@ -0,0 +1,26 @@
+float pattern (vec2 st) {
+    return fract(sin(dot(st.xy, st.xy))*
+        1000. * iGlobalTime);
+}
+
+float random (vec2 st) {
+    return fract(sin(dot(st.xy,
+                         vec2(12.9898,78.233)))*
+        43758.5453123);
+}
+
+void main() {
+    vec2 st = gl_FragCoord.xy/iResolution.xy;
+
+    st *= 10.0; // Scale the coordinate system by 10
+    vec2 ipos = floor(st);  // get the integer coords
+    vec2 fpos = fract(st);  // get the fractional coords
+
+    // Assign a random value based on the integer coord
+    vec3 color = vec3(random( ipos ));
+
+    // Uncomment to see the subdivided grid
+    //color = vec3(fpos,0.0);
+
+    gl_FragColor = vec4(color,1.0);
+}
