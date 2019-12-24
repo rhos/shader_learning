0e0a7f76e4ef81488df0fbcd95a7007f5f2e9dea
ilyas tazitdinov <itazitdinov@acumatica.com>
2019-12-24 17:33:16 +0300

    misc

--- simple.glsl
+++ simple.glsl
@@ -7,17 +7,30 @@ float plot(vec2 coords, float y) {
   return smoothstep(y - 0.02, y, coords.y) - smoothstep(y, y + 0.02, coords.y);
 }
 
+float rand(float a)
+{
+  return fract(sin(a)*1.784);
+}
+
 void main() {
   vec2 st = gl_FragCoord.xy / iResolution.xy;
-  st = (st - .5) * 4.;
+  st = (st - .5) * 15.;
   float x = st.x;
 
-  //float y = sin(st.x*3.14159265359);
+  // float y = sin(st.x*3.14159265359);
+  float i = floor(x);  
+  float f = fract(x);  
+  // y = rand(i);
+  // y = mix(rand(i), rand(i + 1.0), f);
+  // y = mix(rand(i), rand(i + 1.0), smoothstep(0.,1.,f));
+  float u = f * f * (3.0 - 2.0 * f ); 
+  float y = mix(rand(i), rand(i + 1.0), smoothstep(0.25,0.75,f)) * .5; 
+  // y = random(st);
   //float y = 1. - pow(abs(x), .5);
   //float y = pow(cos(PI *x), 0.5);
-  float y = step(0.5, st.x);
+  //float y = step(0.5, st.x);
   // Plot a line
-  float pct = plot(st, y);
+  float pct = plot(st, y) + plot(st, 1.f) + plot(st, 0.f) + plot(st, -1.f);
   vec3 color = (1.0 - pct) * vec3(0.0, 0.0, 0.0) + pct * vec3(0.0, .5, .5);
 
   gl_FragColor = vec4(color, 1.0);
